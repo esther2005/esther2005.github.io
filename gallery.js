faee9c0b90d7753fc7c990fc4e9ada9dade79558
eunseo112233 <eunseo112233@gmail.com>
2017-06-12 21:57:22 -0400

    fix everything

--- gallery.js
+++ gallery.js
@@ -1,6 +1,5 @@
 var pics = ["https://s-media-cache-ak0.pinimg.com/564x/67/ba/f6/67baf623155ca9f55bd501f8ced637a9.jpg",
             "https://s-media-cache-ak0.pinimg.com/originals/1d/28/7d/1d287d08903e8cc1be98f661e4321953.jpg",
-            "http://studyabroad.pk/images/stanforduniversity-video-tour.jpg",
             "http://cdn.hercampus.com/s3fs-public/2016/11/19/bruween.jpg",
             "https://s-media-cache-ak0.pinimg.com/originals/88/c7/05/88c705ebb72a9800fa192de044082ab4.jpg",
             "http://bayimages.net/images/25k/bay024408.jpg"     
